3aba0ad0e59ed1d279f4f14462695211f9f59936
Jyoti-0405 <jyoti.b0405@gmail.com>
2024-05-05 14:49:28 +0530

    first commit

---
+++ MFPE-Audit-Management-master/Audit-Authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Riya Goel', 'riya', 'riya1234', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Yash Sharma', 'yash', 'yash1234', 'Project-2');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Dhanushyaa VS', 'dhanushyaa', 'dhanu1234', 'Project-3');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Jyoti Kumari', 'jyoti', 'jyoti1234', 'Project-4');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Muhammad Iqbal', 'iqbal', 'iqbal1234', 'Project-5');
